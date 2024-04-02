45d80bf3928a095310f4e4a966c87da28da19f39
kalabgebrehiwot <154657840+kalabgebrehiwot@users.noreply.github.com>
2024-04-02 14:11:22 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,13 @@ const linkAction = () =>{
 navlink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const blurHeader = ()=>{
+  const header = document.getElementById('header')
+  //when the scroll is greater than 50 viewport height,aadd the scroll-header class to the header tag//
+  this.scrollY >= 50 ? header.classList.add('blur-header')
+                     : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
